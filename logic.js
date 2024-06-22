0d5f2103617a0a0f04341f343d651a2802ea680c
DennisNeu <76845672+DennisNeu@users.noreply.github.com>
2024-06-22 14:48:21 +0200

    Add logic for dynamic grid and corresponding css
    Is done in rows first to ensure that the tiles
    keep a square shape.

--- logic.js
+++ logic.js
@@ -1,21 +1,25 @@
 const container = document.querySelector(".grid-container");
 
 function generateTiles(tilesNumber) {
-    if (isNaN(tilesNumber)) {
-        alert("Please enter a postive number!");
-        return;
-    }
-    if (tilesNumber < 0) {
-        alert("Please enter a postive number!");
-        return;
-    }
-
-    for (i = 0; i < tilesNumber; i++) {
-        const div = document.createElement("div");
-        div.classList.add("tile");
+    const rows = document.querySelectorAll(".row");
     
-        container.appendChild(div);
-    } 
+    rows.forEach((row) => {
+        for (i = 0; i < tilesNumber; i++) {
+            const div = document.createElement("div");
+            div.classList.add("tile");
+
+            row.appendChild(div);
+        }
+    });
+}
+
+function generateRows(rowsNumber) {
+    for (i = 0; i < rowsNumber; i++) {
+        const div = document.createElement("div");
+        div.classList.add("row");
+
+        container.appendChild(div)
+    }
 }
 
 function deleteTiles() {
@@ -26,4 +30,5 @@ function deleteTiles() {
     });
 }
 
-generateTiles(12);
+generateRows(64);
+generateTiles(64);
